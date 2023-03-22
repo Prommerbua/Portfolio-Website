b6a77a577e0d18645ef9a0dee68fb3d4d9574cee
Michael Prommer <michael.prommer@outlook.de>
2023-03-22 15:07:32 +0100

    Bootstrap Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './Assets/Styles/index.scss';
+import Navigation from './Components/Navbar';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Navigation/>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        <Button>Klick</Button>
+        </header>
+      </div>
   );
 }
 

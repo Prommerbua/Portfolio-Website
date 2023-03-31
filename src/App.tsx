78464a768f6cf869e905844072db9aaab41088f2
Michael Prommer <michael.prommer@outlook.de>
2023-03-31 12:38:25 +0200

    Basic Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 // import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './Assets/Styles/index.scss';
 import Navigation from './Components/Navbar';
 import { Col, Collapse, Container, Nav, Navbar } from 'react-bootstrap';
@@ -11,9 +12,13 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-
-      <Home />
-
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<Home />} />
+          <Route path='/projects' element={<Home />} />
+          <Route path='/cv' element={<Home />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
       <Footer />
     </div >
   );
